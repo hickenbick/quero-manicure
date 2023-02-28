237c24f8880e66ace539e24693e80cee4fbd83f6
Rodrigo Hickenbick <rodrigo.hickenbick@gmail.com>
2023-02-28 15:35:48 +0000

    Depoimentos partially concluded

---
+++ script.js
@@ -0,0 +1,45 @@
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
+const gallery = document.querySelector(".gallery")
+
+controls.forEach(control => {
+  control.addEventListener("click", () => {
+    const isLeft = control.classList.contains("arrow-left");
+    const photoW = items[currentItem].clientWidth;
+    const leftP = items[currentItem].offsetLeft;
+
+    if (isLeft) {
+      currentItem -= 1;
+    } else {
+      currentItem += 1;
+    }
+
+    if (currentItem >= (maxItems -1)) {
+      currentItem -= 1;
+    }
+
+    if (currentItem < 0) {
+      currentItem += 1;
+    }
+
+    items.forEach(item => item.classList.remove("current-item"));
+
+    // items[currentItem].scrollIntoView({
+    //   inline:"start"
+    // })
+
+    if (isLeft) {
+      gallery.scrollBy({
+        left: -photoW,
+        behavior: 'smooth'
+      });
+    } else {
+      gallery.scrollBy({
+        left: photoW,
+        behavior: 'smooth'
+      });
+    }
+  })
+});
